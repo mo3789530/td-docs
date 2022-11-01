1d046ce534e287be22493d64f2d11946cfa59d47
mo <mo3789530@gmail.com>
2022-11-01 23:41:59 +0900

    add args

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
+import argparse
 
-def main():
-    print("start")
-    pass
+
+def main(args):
+    print(args.file)
+    print(args.format)
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file")
+    parser.add_argument("--format")
+    args = parser.parse_args()
+    main(args=args)

038915af58b5667ef0fcf77cbf4afb86bc2fea51
mo <mo3789530@gmail.com>
2022-11-02 22:48:40 +0900

    wip

--- src/main.py
+++ src/main.py
@@ -1,9 +1,30 @@
 import argparse
+import json
+
+import provider.terraform.state as State
+
+
+def json_open(file_path: str) -> dict:
+    json_data = open(file=file_path, mode='r')
+    return json.load(json_data)
+
+
+def parse(data: dict):
+    pass
 
 
 def main(args):
-    print(args.file)
-    print(args.format)
+    print(f'start tf-doc file: {args.file}')
+    # print(args.file)
+    # print(args.format)
+    data = None
+    try:
+        data = json_open(args.file)
+        data.get("resources")
+        print(data.get("resources"))
+    except Exception as e:
+        print(e)
+        exit(-1)
 
 
 if __name__ == "__main__":

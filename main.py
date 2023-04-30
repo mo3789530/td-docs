a8cfc198ef918166c2ec17430863bbb15775324b
mo <mo3789530@gmail.com>
2023-04-30 11:25:36 +0900

    add s3

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import re
 from logging import config, getLogger
 from typing import Optional
+from src.libs.s3 import get_list_buckets_with_prefix
 
 from src.libs.md import is_exist_md, write_md
 from src.service.aws import AWSService
@@ -14,14 +15,14 @@ logger = getLogger(__name__)
 
 
 def json_open(file_path: str) -> dict:
-    json_data = open(file=file_path, mode='r')
+    json_data = open(file=file_path, mode="r")
     return json.load(json_data)
 
 
 def output(file: str, output: Optional[str], format: Optional[str], data: str):
     # file check
     if output == None:
-        if format == 'html':
+        if format == "html":
             output = re.sub(".json", ".html", file)
         else:
             output = re.sub(".json", ".md", file)
@@ -36,7 +37,7 @@ def output(file: str, output: Optional[str], format: Optional[str], data: str):
 
 
 def main(args):
-    logger.info(f'start tf-doc file: {args.file}')
+    logger.info(f"start tf-doc file: {args.file}")
     data = None
 
     try:
@@ -48,7 +49,7 @@ def main(args):
     data = State().parse(data).get("resources", [])
 
     # soted by type
-    data = sorted(data, key=lambda x: x['type'])
+    data = sorted(data, key=lambda x: x["type"])
 
     aws = AWSService()
     result = ""
@@ -65,13 +66,18 @@ def main(args):
     output(file=args.file, output=args.output, format=args.format, data=result)
 
 
+def test_main():
+    res = get_list_buckets_with_prefix("test")
+    print(res)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--file")
     parser.add_argument("--format")
     parser.add_argument("--output")
     args = parser.parse_args()
-    with open("./log_config.json", 'r') as f:
+    with open("./log_config.json", "r") as f:
         log_conf = json.load(f)
     config.dictConfig(log_conf)
     main(args=args)
--- src/libs/s3.py
+++ src/libs/s3.py
@@ -1,6 +1,12 @@
 import boto3
+import os
 
-s3 = boto3.resource('s3')
+s3 = boto3.resource(
+    "s3",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID", ""),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY", ""),
+    endpoint_url=os.getenv("S3_ENDPOINT_URL", ""),
+)
 
 
 def get_list_objects(bucket_name):
@@ -18,4 +24,4 @@ def get_list_buckets_with_prefix(prefix):
 
 def get_object_by_key(bucket_name, key):
     obj = s3.Object(bucket_name, key)
-    return obj.get()['Body'].read().decode('utf-8')
+    return obj.get()["Body"].read().decode("utf-8")
